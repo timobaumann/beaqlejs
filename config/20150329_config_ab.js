fe0d0ca280ef0a17ecd0b6792aa0d4bc27b5e762
Timo Baumann <baumann@informatik.uni-hamburg.de>
2016-06-25 13:24:36 +0200

    preference test; include example configuration

---
+++ config/20150329_config_ab.js
@@ -0,0 +1,94 @@
+// configure the test here
+var TestConfig = {
+  "TestName": "Experiment zur Sprecherpräferenz",
+  "LoopByDefault": false,
+  "ShowFileIDs": false,
+  "ShowResults": true,
+  "EnableABLoop": true,
+  "EnableOnlineSubmission": true,
+  "BeaqleServiceURL": "http://www.timobaumann.de/temp/beaqle/web_service/beaqleJS_Service.php",
+  "SupervisorContact": "baumann@informatik.uni-hamburg.de", 
+  "Testsets": [
+    //    
+    {
+      "Name": "",
+      "TestID": "a0129",
+      "Files": {
+            "A": "audio/standardehmm_arctic_a0129.wav",
+            "B": "audio/standardlab_arctic_a0129.wav",
+      }
+    },
+    {
+      "Name": "",
+      "TestID": "a0188",
+      "Files": {
+            "A": "audio/standardehmm_arctic_a0188.wav",
+            "B": "audio/standardlab_arctic_a0188.wav",
+      }
+    },
+    {
+      "Name": "",
+      "TestID": "a0246",
+      "Files": {
+            "A": "audio/standardehmm_arctic_a0246.wav",
+            "B": "audio/standardlab_arctic_a0246.wav",
+      }
+    },
+    {
+      "Name": "",
+      "TestID": "a0515",
+      "Files": {
+            "A": "audio/standardehmm_arctic_a0515.wav",
+            "B": "audio/standardlab_arctic_a0515.wav",
+      }
+    },
+    {
+      "Name": "",
+      "TestID": "a0590",
+      "Files": {
+            "A": "audio/standardehmm_arctic_a0590.wav",
+            "B": "audio/standardlab_arctic_a0590.wav",
+      }
+    },
+    {
+      "Name": "",
+      "TestID": "b0016",
+      "Files": {
+            "A": "audio/standardehmm_arctic_b0016.wav",
+            "B": "audio/standardlab_arctic_b0016.wav",
+      }
+    },
+    {
+      "Name": "",
+      "TestID": "b0020",
+      "Files": {
+            "A": "audio/standardehmm_arctic_b0020.wav",
+            "B": "audio/standardlab_arctic_b0020.wav",
+      }
+    },
+    {
+      "Name": "",
+      "TestID": "b0264",
+      "Files": {
+            "A": "audio/standardehmm_arctic_b0264.wav",
+            "B": "audio/standardlab_arctic_b0264.wav",
+      }
+    },
+    {
+      "Name": "",
+      "TestID": "b0350",
+      "Files": {
+            "A": "audio/standardehmm_arctic_b0350.wav",
+            "B": "audio/standardlab_arctic_b0350.wav",
+      }
+    },
+    {
+      "Name": "",
+      "TestID": "b0364",
+      "Files": {
+            "A": "audio/standardehmm_arctic_b0364.wav",
+            "B": "audio/standardlab_arctic_b0364.wav",
+      }
+    },
+  ]
+}
